0d9862d2433c0bf148d8951489edb0bc95894825
Pedro Boechat <pboechat@gmail.com>
2017-06-20 14:55:12 +0200

    Update obb.py

--- pyobb/obb.py
+++ pyobb/obb.py
@@ -3,7 +3,7 @@ from numpy.linalg import eigh, norm
 
 
 ########################################################################################################################
-# adapted from: http://jamesgregson.blogspot.co.at/2011/03/latex-test.html
+# adapted from: http://jamesgregson.blogspot.com/2011/03/latex-test.html
 ########################################################################################################################
 class OBB:
     def __init__(self):
