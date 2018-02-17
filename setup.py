f9b19504161429cd07f750599baf648ada37acc8
Pedro Boechat <pboechat@bongfish.com>
2018-02-17 20:25:32 +0100

    stopped reading requirements.txt in setup.py

--- setup.py
+++ setup.py
@@ -3,21 +3,10 @@
 from setuptools import setup
 
 
-def get_requirements():
-    with open('requirements.txt', 'r') as requirements_file:
-        lines = requirements_file.readlines()
-    requirements = []
-    for line in lines:
-        if line.startswith('#'):
-            continue
-        requirements.append(line.strip())
-    return requirements
-
-
 setup(name='pyobb',
       packages=['pyobb'],
       version='1.0',
-      install_requires=get_requirements(),
+      install_requires=['numpy>=1.12'],
       description='Python OBB Implementation',
       author='Pedro Boechat',
       author_email='pboechat@gmail.com',
