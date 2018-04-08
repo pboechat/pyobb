66f56ae032c5948201a256e345dd89c2e4c74af0
Pedro Boechat <pboechat@bongfish.com>
2018-04-08 22:28:09 +0200

    updated version to 1.0.2

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup
 
 setup(name='pyobb',
       packages=['pyobb'],
-      version='1.0.1',
+      version='1.0.2',
       install_requires=['numpy>=1.12'],
       description='Python OBB Implementation',
       author='Pedro Boechat',
       author_email='pboechat@gmail.com',
       url='https://github.com/pboechat/pyobb',
-      download_url = 'https://github.com/pboechat/pyobb/archive/1.0.1.tar.gz',
+      download_url = 'https://github.com/pboechat/pyobb/archive/1.0.2.tar.gz',
       keywords = ['obb', 'computational-geometry', 'oriented-bounding-box'])
